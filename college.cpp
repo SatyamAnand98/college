871d770fb1075fa8fbc1057cc45a74a813ee3cf1
Satyam <thesatemail@gmail.com>
2018-07-30 01:03:16 +0530

    third trial

--- college.cpp
+++ college.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <stdio.h>
+#include <string>
+#include <sstream>
 
 using namespace std;
 struct college
@@ -10,21 +13,35 @@ struct college
     };
 int main()
 {
-    int i=0,cs=0,it=0;
+    int i=0,cs=0,it=0,j=0;
     ifstream coll;
-    coll.open("C:/Users/silveruserDell/Desktop/College2.txt");
-    string nm;
+    coll.open("C:/Users/silveruserDell/Desktop/College.txt");
+    string nm,str,nm2;
     college s[100];
     college s2;
-
-    while(coll>>cs>>it>>nm)
+    while (getline(coll, nm))
     {
-        s[i].name=nm;
-        s[i].cse=cs;
-        s[i].ise=it;
-        i++;
+        int l=nm.length();
+        for(i=0;i<l;i++)
+        {
+            if (isdigit(nm[i]))
+            {
+                str=nm.substr(0,i);
+                s[j].name=str;
+                nm2=nm.substr(i,l);
+                stringstream num(nm2);
+                while(num>>cs>>it)
+                {
+                    s[j].cse=cs;
+                    s[j].ise=it;
+                }
+                break;
+            }
+        }
+        j++;
     }
-    int n=i;
+
+    int n=j;
     ofstream colle;
     colle.open("C:/Users/silveruserDell/Desktop/outputdirectory.txt");
 
@@ -40,16 +57,16 @@ int main()
                 }
             }
         }
-        colle<<"------------------------------------------------------------------------------"<<endl;
-        colle<<"\t\t\t\t  College List(CSE)"<<endl;
-        colle<<"------------------------------------------------------------------------------"<<endl;
-        colle<<"College name \t\t\t\t CSE \t\t\t\t ISE"<<endl;
-        colle<<"------------------------------------------------------------------------------"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
+        colle<<"\t\t\t\t\t\t  College List(CSE)"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
+        colle<<"College name \t\t\t\t\t\t\t CSE \t\t\t\t ISE"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
         for(int k=0;k<n;k++)
         {
-            colle<<s[k].name<<"\t\t\t\t\t"<<s[k].cse<<"\t\t\t\t"<<s[k].ise<<endl;
+            colle<<s[k].name<<"\t"<<s[k].cse<<"\t\t\t\t"<<s[k].ise<<endl<<endl;
         }
-        colle<<"------------------------------------------------------------------------------"<<endl<<endl<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl<<endl<<endl;
         for(int j=0;j<n;j++)
         {
             for(int i=0;i<n-1;i++)
@@ -62,15 +79,15 @@ int main()
                 }
             }
         }
-        colle<<"------------------------------------------------------------------------------"<<endl;
-        colle<<"\t\t\t\t  College List(ISE)"<<endl;
-        colle<<"------------------------------------------------------------------------------"<<endl;
-        colle<<"College name \t\t\t\t CSE \t\t\t\t ISE"<<endl;
-        colle<<"------------------------------------------------------------------------------"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
+        colle<<"\t\t\t\t\t\t  College List(ISE)"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
+        colle<<"College name \t\t\t\t\t\t\t CSE \t\t\t\t ISE"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
         for(int i=0;i<n;i++)
         {
-            colle<<s[i].name<<"\t\t\t\t\t"<<s[i].cse<<"\t\t\t\t"<<s[i].ise<<endl;
+            colle<<s[i].name<<"\t"<<s[i].cse<<"\t\t\t\t"<<s[i].ise<<endl<<endl;
         }
-        colle<<"------------------------------------------------------------------------------"<<endl;
+        colle<<"----------------------------------------------------------------------------------------------------------"<<endl;
     return 0;
 }
